dab8a10f2c6b794b3f30f8c9f3142864bdd59d2c
Aldrin <aldrin.s.gravides@framgia.com>
2016-02-19 11:31:07 +0800

    Add full_title method in application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(string = "")
+    basic_title = "Ruby on Rails Tutorial"
+    if string.blank?
+      basic_title
+    else
+      "#{string} | Ruby on Rails Tutorial"
+    end
+  end
 end
