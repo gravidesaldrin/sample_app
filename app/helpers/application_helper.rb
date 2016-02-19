4fd9d21731c46e252f2fbd7514c16a9aac2e5b1d
Aldrin <aldrin.s.gravides@framgia.com>
2016-02-19 14:02:37 +0800

    Finish Filling in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def full_title(string = "")
-    basic_title = "Ruby on Rails Tutorial"
+    basic_title = "Ruby on Rails Tutorial Sample App"
     if string.blank?
       basic_title
     else
-      "#{string} | Ruby on Rails Tutorial"
+      "#{string} | Ruby on Rails Tutorial Sample App"
     end
   end
 end
